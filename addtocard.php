96970b25a673ae6293d1899854216c91ee33a601
PrajapatiPrince2005 <prajapatip.j330@gmail.com>
2025-02-18 20:23:38 +0530

    Work On Struture Formet Work

---
+++ addtocard.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add To Card</title>
+</head>
+<body>
+<?php include "../SportHub/include/after_login.php"?>
+
+
+</body>
+</html>
